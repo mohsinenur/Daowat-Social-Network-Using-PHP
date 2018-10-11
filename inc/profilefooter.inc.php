6cbd53ce22ab9d8cb0b2a161cc22d96ee3145154
Nur Mohsin <follow.mohsin@gmail.com>
2018-10-11 09:11:01 +0600

    Update profilefooter.inc.php

--- inc/profilefooter.inc.php
+++ inc/profilefooter.inc.php
@@ -3,7 +3,7 @@
 		<a href="#" style="text-decoration: none; color: #7C7979;" >Help  -</a>
 		<a href="#" style="text-decoration: none; color: #7C7979;" >Invite  -</a>
 		<a href="#" style="text-decoration: none; color: #7C7979;" >FAQ  -</a>
-		<a href="#" style="text-decoration: none; color: #7C7979;" >Privacy  -</a>
-		<a href="#" style="text-decoration: none; color: #7C7979;" >Developers</a>
-	<p style="text-align: center; padding: 10px 0px;">Mohsin E Nur production &copy; 2016 | Daowat</p>
+		<a href="https://www.youtube.com/mohsingram" style="text-decoration: none; color: #7C7979;" >Youtube  -</a>
+		<a href="https://www.facebook.com/mohsingram/" style="text-decoration: none; color: #7C7979;" >Facebook</a>
+	<p href="https://www.facebook.com/mohsingram/" style="text-align: center; padding: 10px 0px;">Mohsin Gram &copy; 2016 | Daowat</p>
 </div>
